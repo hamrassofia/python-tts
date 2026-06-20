94dda53cfd24bc8fde1ccbe883f1189bca5309fc
hamrassofia <hamrassofia@gmail.com>
2026-06-20 03:03:31 +0100

    correction de la syntaxe et test reussi

--- sofia.py
+++ sofia.py
@@ -1,17 +1,19 @@
 from gtts import gTTS
 import os
-txt = input("Enter the text you want to convert to speech: ")
-print("choose your language :")
-print("en = English")
-print("fr = French")
-print("es = Spanish")
-Lang = input("Enter the language code: ").strip().lower()
-if Lang not in ['en', 'fr', 'es']:
-    print("Invalid language code. Defaulting to English.")
-    Lang = 'en'
+
+try: 
+    txt = input("Enter the text you want to convert to speech: ")
+    print("choose your language :")
+    print("en = English")
+    print("fr = French")
+    print("es = Spanish")
+    Lang = input("Enter the language code: ").strip().lower()
+    if Lang not in ['en', 'fr', 'es']:
+      print("Invalid language code. Defaulting to English.")
+      Lang = 'en'
     print("Converting text to speech...")
-res = gTTS(text=txt, lang=Lang)
-res.save("audio.mp3")
-print("Audio file saved as audio.mp3")
+    res = gTTS(text=txt, lang=Lang)
+    res.save("audio.mp3")
+    print("Audio file saved as audio.mp3")
 except Exception as e:
     print(f"An error occurred: {e}")
